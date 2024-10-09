c026eeb8577442d245f6925a155b55ddcad82a4f
kd2ex <kountdown@mail.ru>
2024-10-09 14:22:51 +0300

    Player's collected food are now displays in collectable panel

--- Assets/6_Scripts/UI/Collectables/CollectablesPanel.cs
+++ Assets/6_Scripts/UI/Collectables/CollectablesPanel.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class CollectablesPanel : MonoBehaviour
 {
     [SerializeField] private InputReader input;
-
+    [SerializeField] private GameObject FoodLine;
+    
     private void OnEnable()
     {
+        FoodLine.SetActive(true);
         input.DisableGameplayInput();
         Time.timeScale = 0f;
     }
 
     private void OnDisable()
     {
+        FoodLine.SetActive(false);
         input.EnableGameplayInput();
         Time.timeScale = 1f;
     }
