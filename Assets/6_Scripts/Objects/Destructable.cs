cc5af6aadbabaa875049f277367e61176094a788
kd2ex <kountdown@mail.ru>
2024-10-26 20:13:48 +0300

    desctructables sound crushes fixed

--- Assets/6_Scripts/Objects/Destructable.cs
+++ Assets/6_Scripts/Objects/Destructable.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Destructable : MonoBehaviour
 {
+    [SerializeField] private AudioSource source;
+    
     private Collider2D m_Collider;
     private Animator m_Animator;
     
@@ -15,6 +17,7 @@ public class Destructable : MonoBehaviour
     {
         m_Collider.enabled = false;
         m_Animator.SetTrigger("Destroy");
+        source.Play();
         // play sound
     }
 }
