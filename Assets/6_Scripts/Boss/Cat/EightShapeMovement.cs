67354a4b99e3c1a291c57c008c61d98fcd6180ec
kd2ex <kountdown@mail.ru>
2024-10-28 00:10:49 +0300

    Movement for cats heads added

---
+++ Assets/6_Scripts/Boss/Cat/EightShapeMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EightShapeMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    [SerializeField] private bool reverse;
+    
+    private Vector3 offset;
+    
+    private void Start()
+    {
+        offset = transform.position;
+    }
+
+    private float t;
+    private float xPos;
+    private float yPos;
+    
+    private void Update()
+    {
+        var multiplier = reverse ? -1f : 1f;
+
+        t += speed * Time.deltaTime;
+        xPos = Mathf.Cos(t) * multiplier;                        
+        yPos = Mathf.Sin(2 * t) / 2;
+
+        transform.position = new Vector3(xPos, yPos, 0f) + offset;
+    }
+}
--- Assets/6_Scripts/Utils/Coroutines.cs
+++ Assets/6_Scripts/Utils/Coroutines.cs
@@ -10,4 +10,37 @@ public class Coroutines
         yield return new WaitForSeconds(seconds);
         after?.Invoke();
     }
+    
+    private IEnumerator MoveTo(Transform to, Transform origin, float speed, float delay = 0f, Action after = null)
+    {
+        var waiter = new WaitForFixedUpdate();
+        var Pos = origin.position;
+        
+        var dir = Vector3.Normalize((to.position - Pos).normalized);
+        var dist = GetDistance();
+
+        while (dist > .1f)
+        {
+            dist = GetDistance();
+            origin.Translate(dir * (speed * Time.fixedDeltaTime));
+            
+            yield return waiter;
+        }
+
+        var elapsed = 0f;
+
+        while (elapsed < delay)
+        {
+            elapsed += Time.fixedDeltaTime;
+            yield return waiter;
+        }
+
+        after?.Invoke();
+        yield break;
+
+        float GetDistance()
+        {
+            return (to.position - Pos).magnitude;
+        }
+    }
 }
