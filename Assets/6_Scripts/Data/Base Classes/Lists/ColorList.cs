eac35afbe638c4fb77953ebc53996419de87629a
kd2ex <kountdown@mail.ru>
2024-11-18 18:44:36 +0300

    blood panel fixed

---
+++ Assets/6_Scripts/Data/Base Classes/Lists/ColorList.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Color List", menuName = "Scriptable Objects/Data/Color list")]
+public class ColorList : SOListData<Color>
+{
+}
---
+++ Assets/6_Scripts/UI/NemoGrownPanel/BloodPanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodPanel : MonoBehaviour
+{
+    [SerializeField] private SOListData<Color> colors;
+    [SerializeField] private Image image;
+    
+    private int levelIndex => GameManager.instance.NemoCurrentLevel;
+    
+    private void OnEnable()
+    {
+        image.color = colors.Items[levelIndex];
+    }
+}
