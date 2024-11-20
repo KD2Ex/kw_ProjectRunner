8c0b076190c63b79d9cd7936229e3813ef334db6
kd2ex <kountdown@mail.ru>
2024-11-20 19:43:50 +0300

    visual bug fixes

--- Assets/6_Scripts/Parallax/BecomeParallax.cs
+++ Assets/6_Scripts/Parallax/BecomeParallax.cs
@@ -1,26 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
 
 public class BecomeParallax : MonoBehaviour
 {
-    private void Awake()
-    {
-
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
         transform.position = transform.parent.position;
         transform.SetParent(null);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
