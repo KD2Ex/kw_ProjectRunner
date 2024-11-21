1bb34ec074dd34e722edd60541e235bc926253dd
kd2ex <kountdown@mail.ru>
2024-11-21 19:08:29 +0300

    bug fix in chunk

---
+++ Assets/6_Scripts/Achievments/Acheivements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public struct AchievmentsConditions
+{
+    public string name;
+    public Func<bool> condition;
+}
+
+public class Achievements
+{
+    public List<Achievement> achievements = new ();
+
+
+    private void CompleteLocationOne()
+    {
+        achievements[0].Completed = true;
+    }
+}
+
+[Serializable]
+public class Achievement
+{
+    public string Title;
+    public string Description;
+    public Image Icon;
+    public Func<bool> Condition;
+    public bool Completed;
+}   
